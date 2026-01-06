12c18783e3513159bdb29cda66e5f9322ef691f5
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-06 18:01:29 +0000

    isNaN returns false first, true second

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function calculateSquare(){
     let result = document.getElementById('result');
 
     //add code here
+    if(isNaN(value)){
+      result.textContent = "Invalid, please enter a number";  
+    } else {
+      result.textContent = value * value;
+    }
     
 }
